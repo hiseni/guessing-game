d311eae64534c5735a82937dd95af151ccf7cb3e
Bogdan Arabadzhi <bogdan.today@gmail.com>
2017-01-29 15:47:32 +0300

    Implement basic GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,39 @@
 class GuessingGame {
-    constructor() {}
+    // We can set an initial range using constructor params
+    constructor(min = 0, max = 0) {
+        this.setRange(min, max);
+    }
 
     setRange(min, max) {
+        // We only deal with safe integers, nobody will think of floats :D
+        if (!Number.isSafeInteger(min) || !Number.isSafeInteger(max)) {
+            throw new RangeError('Parameters must be safe integers');
+        }
 
+        // Ah-ha, no breaking ranges!
+        if (min > max) {
+            [ min, max ] = [ max, min ];
+        }
+
+        this.min = min;
+        this.max = max;
+
+        // We are not guessing yet
+        this.answer = 0;
     }
 
     guess() {
+        this.answer = Math.round((this.min + this.max) / 2);
 
+        return this.answer;
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer;
     }
 }
 
